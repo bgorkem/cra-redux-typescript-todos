9f25ca21a77676ed39b3827cbc2b6d682178d922
Bulent Gorkem <bulent.gorkem@nwmworld.com>
2019-10-13 19:44:32 +0100

    Updates

---
+++ precache-manifest.85c2ee76dbfd56e445f4e70d88a44a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36073362e63e4502f5d3fe8a562e18ed",
+    "url": "/cra-redux-typescript-todos/index.html"
+  },
+  {
+    "revision": "9ec381e43180c2fc3524",
+    "url": "/cra-redux-typescript-todos/static/css/main.d46cda80.chunk.css"
+  },
+  {
+    "revision": "8a02702e1b1f115d52ab",
+    "url": "/cra-redux-typescript-todos/static/js/2.977809b3.chunk.js"
+  },
+  {
+    "revision": "9ec381e43180c2fc3524",
+    "url": "/cra-redux-typescript-todos/static/js/main.0f476aae.chunk.js"
+  },
+  {
+    "revision": "b1cd570894d9463ec5ed",
+    "url": "/cra-redux-typescript-todos/static/js/runtime-main.27def3cf.js"
+  }
+]);
